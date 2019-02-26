da2c8c3d5aa7823a27d115f4be035e3220b7c3d2
Roopam Sharma <roopamsharma49@gmail.com>
2019-02-26 02:02:13 -0600

    Added the mongodb for storing the user data.

--- chat.js
+++ chat.js
@@ -3,39 +3,73 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var session = require('express-session')
 const bodyParser = require('body-parser');
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/chat.html');
-});
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'mydb';
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.io = io;
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
-}))
+}));
+
+app.get('/', function(req, res){
+  console.log('user connected');
+  res.sendFile(__dirname + '/chat.html');
+});
 
 app.post('/addname', function(req, res){
-  console.log(req.body);
-  req.session.username = req.body.name;
-  res.send("added: "+req.session.username);
+  var name = req.body.name;
+  req.session.username = name;
+  req.app.io.emit('addtab',name);
+  res.send("added: "+name);
 });
 
 app.post('/checkname', function(req, res){
-  console.log(req.session.username);
   res.send(req.session.username);
 });
 
 io.on('connection', function(socket){
-  console.log('user connected');
+  socket.on('addtab',function(name){
+    const dbclient = new MongoClient(url,{useNewUrlParser: true});
+    dbclient.connect(function(err) {
+      const db = dbclient.db(dbName);
+      var obj = {'name':name,'tabid':[socket.id]};
+      db.collection("users").find({'name':name}).toArray(function(err,result) {
+          if (err) throw err;
+          for(var i=0;i<result.length;i++){
+            console.log("name: "+result[i]['name']);
+              if(result[i]['name']==name){
+                obj['tabid'] = socket.id;
+                //send message to result[i]['tabid'] to redirect to other.html
+                break;
+              }
+          }
+          console.log("new object: ");
+          console.log(obj);
+          db.collection("users").insertOne(obj, function(err,res) {
+              if (err) throw err;
+                console.log("1 document inserted");
+          });
+          dbclient.close();
+          });
+    });
+  });
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-  socket.on('disconnect', function(msg){
-    console.log(socket.id);
+  socket.on('disconnect', function(){
+    console.log('disconnected: '+socket.id);
     io.emit('chat message','someone left');
-    console.log('user disconnected');
   });
 });
 
--- delete_collection.js
+++ delete_collection.js
@@ -4,6 +4,10 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
+  dbo.collection("users").drop(function(err,st){
+      if(st) console.log("Collection deleted");
+  
+  });	
   dbo.createCollection("users", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
